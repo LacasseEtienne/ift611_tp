c5b7d3be569419639058cfbb49fa16fb4667fbdc
Thimble <thimbledice@gmail.com>
2022-03-13 14:45:25 -0400

    server listener

--- server/main.cpp
+++ server/main.cpp
@@ -1,18 +1,121 @@
-#include <boost/lambda/lambda.hpp>
+#include <cstdlib>
 #include <iostream>
-#include <iterator>
-#include <algorithm>
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
 
-int main()
-{
+namespace net = boost::asio;
+namespace beast = boost::beast;
+using tcp = boost::asio::ip::tcp;
 
-    std::cout << "Hello, World!" << std::endl;
+void fail(beast::error_code ec, char const *what) {
+    std::cerr << what << ": " << ec.message() << "\n";
+}
 
-    using namespace boost::lambda;
-    typedef std::istream_iterator<int> in;
+class listener : public std::enable_shared_from_this<listener> {
+    net::io_context &ioc_;
+    tcp::acceptor acceptor_;
 
-    std::for_each(
-        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+public:
+    listener(net::io_context &ioc, tcp::endpoint endpoint) : ioc_(ioc), acceptor_(ioc) {
+        beast::error_code ec;
 
-    return 0;
+        // Open the acceptor
+        acceptor_.open(endpoint.protocol(), ec);
+        if (ec) {
+            fail(ec, "open");
+            return;
+        }
+
+        // Allow address reuse
+        acceptor_.set_option(net::socket_base::reuse_address(true), ec);
+        if (ec) {
+            fail(ec, "set_option");
+            return;
+        }
+
+        // Bind to the server address
+        acceptor_.bind(endpoint, ec);
+        if (ec) {
+            fail(ec, "bind");
+            return;
+        }
+
+        // Start listening for connections
+        acceptor_.listen(net::socket_base::max_listen_connections, ec);
+        if (ec) {
+            fail(ec, "listen");
+            return;
+        }
+    }
+
+    // Start accepting incoming connections
+    void run() {
+        do_accept();
+    }
+
+private:
+    void do_accept() {
+        // The new connection gets its own strand
+        acceptor_.async_accept(net::make_strand(ioc_),
+                               beast::bind_front_handler(&listener::on_accept, shared_from_this()));
+    }
+
+    void on_accept(beast::error_code ec, tcp::socket socket) {
+        if (ec) {
+            fail(ec, "accept");
+        } else {
+            // Create the session and run it
+            std::cout << "Create the session and run it\n";
+//            std::make_shared<session>(std::move(socket))->run();
+        }
+
+        // Accept another connection
+        do_accept();
+    }
+};
+
+auto checkCommandLineArguments(int argc){
+    if (argc != 4) {
+        std::cerr << "Usage: websocket-server-async <address> <port> <threads>\n" << "Example:\n"
+                  << "    websocket-server-async 0.0.0.0 8080 1\n";
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
+auto createAnIOContext(int threads){
+    return net::io_context{threads};
+}
+
+void createAndLaunchListeningPort(net::io_context &ioc, const net::ip::address address, const unsigned short port){
+    std::make_shared<listener>(ioc, tcp::endpoint{address, port})->run();
+}
+
+void runIOService(const int threads, net::io_context &ioc) {
+    std::vector<std::thread> v;
+    v.reserve(threads - 1);
+    for(auto i = threads - 1; i > 0; --i)
+        v.emplace_back(
+                [&ioc]
+                {
+                    ioc.run();
+                });
+    ioc.run();
+}
+
+int main(int argc, char *argv[]) {
+    if (checkCommandLineArguments(argc) == EXIT_FAILURE){
+        return EXIT_FAILURE;
+    }
+
+    auto const address = net::ip::make_address(argv[1]);
+    auto const port = static_cast<unsigned short>(std::atoi(argv[2]));
+    auto const threads = std::max<int>(1, std::atoi(argv[3]));
+
+    auto ioc = createAnIOContext(threads);
+    createAndLaunchListeningPort(ioc, address, port);
+    runIOService(threads, ioc);
+
+    return EXIT_SUCCESS;
 }
